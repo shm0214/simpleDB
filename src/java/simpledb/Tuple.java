fe67ef6ffcb33a277701123c80545b7122d6d5bf
shm0214 <1186562408@qq.com>
2021-03-21 22:27:34 +0800

    Pass Tuple Test (With modifyRecordId() failed)

--- src/java/simpledb/Tuple.java
+++ src/java/simpledb/Tuple.java
@@ -1,107 +1,131 @@
 package simpledb;
 
 import java.io.Serializable;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
- * Tuple maintains information about the contents of a tuple. Tuples have a
- * specified schema specified by a TupleDesc object and contain Field objects
- * with the data for each field.
+ * Tuple maintains information about the contents of a tuple. Tuples have a specified schema
+ * specified by a TupleDesc object and contain Field objects with the data for each field.
  */
 public class Tuple implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    private TupleDesc tupleDesc;
+    private RecordId recordId;
+    private Field[] fields;
 
     /**
      * Create a new tuple with the specified schema (type).
+     * As long as init the fields array.
      *
-     * @param td
-     *            the schema of this tuple. It must be a valid TupleDesc
-     *            instance with at least one field.
+     * @param td the schema of this tuple. It must be a valid TupleDesc instance with at least one
+     *           field.
      */
     public Tuple(TupleDesc td) {
-        // some code goes here
+        this.tupleDesc = td;
+        fields = new Field[td.numFields()];
     }
 
     /**
      * @return The TupleDesc representing the schema of this tuple.
      */
     public TupleDesc getTupleDesc() {
-        // some code goes here
-        return null;
+        return tupleDesc;
     }
 
     /**
-     * @return The RecordId representing the location of this tuple on disk. May
-     *         be null.
+     * @return The RecordId representing the location of this tuple on disk. May be null.
      */
     public RecordId getRecordId() {
-        // some code goes here
-        return null;
+        return recordId;
     }
 
     /**
      * Set the RecordId information for this tuple.
      *
-     * @param rid
-     *            the new RecordId for this tuple.
+     * @param rid the new RecordId for this tuple.
      */
     public void setRecordId(RecordId rid) {
-        // some code goes here
+        this.recordId = rid;
     }
 
     /**
      * Change the value of the ith field of this tuple.
      *
-     * @param i
-     *            index of the field to change. It must be a valid index.
-     * @param f
-     *            new value for the field.
+     * @param i index of the field to change. It must be a valid index.
+     * @param f new value for the field.
      */
-    public void setField(int i, Field f) {
-        // some code goes here
+    public void setField(int i, Field f) throws IllegalArgumentException {
+        if (i >= this.tupleDesc.numFields() || i < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            this.fields[i] = f;
+        }
     }
 
     /**
+     * @param i field index to return. Must be a valid index.
      * @return the value of the ith field, or null if it has not been set.
-     *
-     * @param i
-     *            field index to return. Must be a valid index.
      */
     public Field getField(int i) {
-        // some code goes here
-        return null;
+        if (i >= this.tupleDesc.numFields() || i < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            return this.fields[i];
+        }
     }
 
     /**
-     * Returns the contents of this Tuple as a string. Note that to pass the
-     * system tests, the format needs to be as follows:
+     * Returns the contents of this Tuple as a string. Note that to pass the system tests, the format
+     * needs to be as follows:
      *
-     * column1\tcolumn2\tcolumn3\t...\tcolumnN
+     * <p>column1\tcolumn2\tcolumn3\t...\tcolumnN
      *
-     * where \t is any whitespace (except a newline)
+     * <p>where \t is any whitespace (except a newline)
      */
+    @Override
     public String toString() {
-        // some code goes here
-        throw new UnsupportedOperationException("Implement this");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < fields.length; i++) {
+            stringBuilder.append(fields[i]);
+            if (i != fields.length - 1) {
+                stringBuilder.append(' ');
+            } else {
+                stringBuilder.append('\n');
+            }
+        }
+        return stringBuilder.toString();
     }
 
     /**
-     * @return
-     *        An iterator which iterates over all the fields of this tuple
-     * */
-    public Iterator<Field> fields()
-    {
-        // some code goes here
-        return null;
+     * @return An iterator which iterates over all the fields of this tuple
+     */
+    public Iterator<Field> fields() {
+        return new Iterator<Field>() {
+            private int position = 0;
+
+            @Override
+            public boolean hasNext() {
+                return position < fields.length;
+
+            }
+
+            @Override
+            public Field next() {
+                if (!hasNext()) {
+                    return null;
+                } else {
+                    return fields[position++];
+                }
+            }
+        };
     }
 
     /**
      * reset the TupleDesc of this tuple (only affecting the TupleDesc)
-     * */
-    public void resetTupleDesc(TupleDesc td)
-    {
-        // some code goes here
+     */
+    public void resetTupleDesc(TupleDesc td) {
+        this.tupleDesc = new TupleDesc(new ArrayList<>());
     }
 }
